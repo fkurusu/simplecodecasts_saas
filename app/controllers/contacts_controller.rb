8d94bffb54ba729308994d0d1646d5f81833ac4c
Fernando Cruz <f.cruzval@gmail.com>
2016-08-01 23:06:29 +0000

    Create action, validations and flash hash

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,18 @@ class ContactsController < ApplicationController
   end
   
   def create
-    
+    @contact = Contact.new(contacts_params)
+    if @contact.save
+      flash[:success] = 'Message sent.'
+      redirect_to new_contact_path
+    else
+      flash[:error] = 'Error ocurred, message has not been sent.'
+      redirect_to new_contact_path
+    end
   end
+  
+  private
+    def contacts_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
