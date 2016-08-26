763ecac2a7f782cba793a0d64a3b410e2e41ae20
Fernando Cruz <f.cruzval@gmail.com>
2016-08-26 16:26:26 +0000

    Added avatar column to Profile table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822042334) do
+ActiveRecord::Schema.define(version: 20160826162413) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -36,8 +36,12 @@ ActiveRecord::Schema.define(version: 20160822042334) do
     t.string   "phone_number"
     t.string   "contact_email"
     t.text     "description"
-    t.datetime "created_at",    limit: 255
-    t.datetime "updated_at",    limit: 255
+    t.datetime "created_at",          limit: 255
+    t.datetime "updated_at",          limit: 255
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: true do |t|
